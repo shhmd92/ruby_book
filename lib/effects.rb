fb891d9435a2da06f95d916bf69c8fd910ec4793
shhmd92 <sho.hamada.92@gmail.com>
2020-07-11 20:42:10 +0900

    [add]reverseとechoの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 module Effects
   def self.reverse
+    lambda do |words|
+      words.split(' ').map(&:reverse).join(' ')
+    end
+  end
 
+  def self.echo(rate)
+    lambda do |words|
+      words.chars.map { |c| c == ' ' ? c : c * rate }.join
+    end
   end
 end
