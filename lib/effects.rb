76b3b7960ec04d22526623c0866a5635bb918cec
shhmd92 <sho.hamada.92@gmail.com>
2020-07-11 20:46:10 +0900

    [add]loudの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -12,4 +12,10 @@ module Effects
       words.chars.map { |c| c == ' ' ? c : c * rate }.join
     end
   end
+
+  def self.loud(level)
+    lambda do |words|
+      words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
+    end
+  end
 end
