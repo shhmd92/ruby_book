218d8cc835cc6b83b3ca98943f67fbe96b0df3d8
shhmd92 <sho.hamada.92@gmail.com>
2020-07-11 20:41:46 +0900

    [add]reverseとechoのテスト

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require './lib/effects'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    assert Effects
+    effect = Effects.reverse
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
+  end
+
+  def test_echo
+    effect = Effects.echo(2)
+    assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+
+    effect = Effects.echo(3)
+    assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
   end
 end
